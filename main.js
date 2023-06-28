dbd1b58120fd42ed5895d5beaa3b843da472c190
AndyMHo <125214805+AndyMHo@users.noreply.github.com>
2023-06-27 23:52:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ let exponent = 0
 function increase() {
     exponent+=1
     document.getElementById("number").innerHTML='2<sup>'+exponent+'</sup>'
+    document.getElementById("equals").innerHTML='='+2**exponent
 }
