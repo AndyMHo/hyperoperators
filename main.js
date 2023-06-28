c9c32540834c4bef44d012698e947033b40b1a87
AndyMHo <125214805+AndyMHo@users.noreply.github.com>
2023-06-28 18:47:17 -0400

    new button

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+let base = 2
 let exponent = 0
+let exponentials = 0
+let increase_per_click = 1
+let tetrationals = 0
 
 function increase() {
     exponent+=1
-    document.getElementById("number").innerHTML='2<sup>'+exponent+'</sup>'
-    document.getElementById("equals").innerHTML='='+2**exponent
+    document.getElementById("number").innerHTML=base+'<sup>'+exponent+'</sup>'
+    document.getElementById("equals").innerHTML='='+base**exponent
+}
+
+function tetrate(params) {
+    
 }
