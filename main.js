329b13c6b9f4c46e78ae0419c6ba88eeb04f796d
AndyMHo <125214805+AndyMHo@users.noreply.github.com>
2023-06-28 19:01:41 -0400

    tetrate

--- main.js
+++ main.js
@@ -5,11 +5,26 @@ let increase_per_click = 1
 let tetrationals = 0
 
 function increase() {
-    exponent+=1
-    document.getElementById("number").innerHTML=base+'<sup>'+exponent+'</sup>'
-    document.getElementById("equals").innerHTML='='+base**exponent
+    exponent += increase_per_click
 }
 
-function tetrate(params) {
-    
+function tetrate() {
+    if (base ** exponent == Infinity) {
+        base += 1
+        exponent = 0
+        exponentials = 0
+        increase_per_click = 1
+        tetrationals += 1
+    }
 }
+
+function update() {
+    document.getElementById("number").innerHTML = base + '<sup>' + exponent + '</sup>'
+    document.getElementById("equals").innerHTML = '=' + base ** exponent
+    document.getElementById("exponentials").innerHTML = 'You have ' + exponentials + ' exponentials.'
+    document.getElementById("tetrationals").innerHTML = 'You have ' + tetrationals + ' tetrationals.'
+}
+
+setInterval(() => {
+    update()
+}, 50);
